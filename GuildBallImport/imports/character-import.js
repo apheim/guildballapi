ed49fe3dc257b90df0d31cad6fa01d9e471548cd
Andrew <Andrew>
2017-02-06 21:51:56 -0600

    Added Playbook Columns Import

--- GuildBallImport/imports/character-import.js
+++ GuildBallImport/imports/character-import.js
@@ -19,6 +19,14 @@ module.exports = function(){
     ARMOR: 9,
     INFLUENCESTART:10,
     INFLUENCEMAX: 11,
+    PB1: 12,
+    PB2: 13,
+    PB3: 14,
+    PB4: 15,
+    PB5: 16,
+    PB6: 17,
+    PB7: 18,
+    PB8: 19,
     HEALTH: 38,
     ICYSPONGE:39
   };
@@ -50,55 +58,256 @@ var getTeam = function(teamName, onSuccess) {
 
 };
 
+function getPlaybookActions(onSuccess){
+    var options = {
+      host: "localhost",
+      port: 3000,
+      path: "/api/PlaybookActions",
+      method: 'GET'
+    };
 
-var parser = parse({delimiter: ',', auto_parse : true}, function (err, data) {
-  async.eachSeries(data, function (line, callback) {
-      if(rowCount++ == 0) {
-        callback();
-        return;
-      }
+    http.get(options, function(res){
+      var bodyChunks = [];
+      res.on('data', function(chunk) {
+        bodyChunks.push(chunk);
+      }).on('end', function() {
 
-      getTeam(line[COLUMNS.TEAM], function(team){
-        console.log(team);
-        var body = JSON.stringify({
-          "Name": line[COLUMNS.NAME],
-          "MeleeZone": line[COLUMNS.MELEEZONE],
-          "Jog": line[COLUMNS.JOG],
-          "Sprint": line[COLUMNS.SPRINT],
-          "TAC": line[COLUMNS.TAC],
-          "KickDice": line[COLUMNS.KICKDICE],
-          "KickLength": line[COLUMNS.KICKLENGTH],
-          "Defense": line[COLUMNS.DEFENSE],
-          "Armor": line[COLUMNS.ARMOR],
-          "InfluenceStart": line[COLUMNS.INFLUENCESTART],
-          "InfluenceMax": line[COLUMNS.INFLUENCEMAX],
-          "IconUrl": line[COLUMNS.NAME],
-          "Health": line[COLUMNS.HEALTH],
-          "IcySponge": line[COLUMNS.ICYSPONGE],
-          "TeamId": team.id
-        });
+        var body = Buffer.concat(bodyChunks);
+        if(bodyChunks)
+          onSuccess(JSON.parse(bodyChunks));
+      })
+    }).end();
 
-        var createOptions = {
-          host: "localhost",
-          port: 3000,
-          path: "/api/characters",
-          method: 'POST',
-          headers: {
-           "Content-Type": "application/json",
-           "Content-Length": Buffer.byteLength(body)
-         }
-        };
+  };
+
+  function createPlaybookColumn(col, onSuccess){
+    col = JSON.stringify(col);
+
+    var createOptions = {
+      host: "localhost",
+      port: 3000,
+      path: "/api/PlaybookColumns",
+      method: 'POST',
+      headers: {
+       "Content-Type": "application/json",
+       "Content-Length": Buffer.byteLength(col)
+     }
+    };
 
 
-        var postreq = http.request(createOptions, function(res) {
-          res.setEncoding('utf8');
-          callback();
-        });
-
-        postreq.write(body);
-        postreq.end();
+    var postreq = http.request(createOptions, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        onSuccess(JSON.parse(chunk));
       });
-  })
-})
-fs.createReadStream(inputFile).pipe(parser);
+    });
+    postreq.write(col);
+    postreq.end();
+  };
+
+  function createPlaybookResult(result, onSuccess){
+    result = JSON.stringify(result);
+
+    var createOptions = {
+      host: "localhost",
+      port: 3000,
+      path: "/api/PlayBookResults",
+      method: 'POST',
+      headers: {
+       "Content-Type": "application/json",
+       "Content-Length": Buffer.byteLength(result)
+     }
+    };
+
+
+    var postreq = http.request(createOptions, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        onSuccess(JSON.parse(chunk));
+      });
+    });
+
+    postreq.write(result);
+    postreq.end();
+  }
+
+  function createPlaybookResultAction(action){
+    action = JSON.stringify(action);
+    var createOptions = {
+      host: "localhost",
+      port: 3000,
+      path: "/api/PlayBookResultActions",
+      method: 'POST',
+      headers: {
+       "Content-Type": "application/json",
+       "Content-Length": Buffer.byteLength(action)
+     }
+    };
+
+
+    var postreq = http.request(createOptions, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        onSuccess(JSON.parse(chunk));
+      });
+    });
+
+    postreq.write(action);
+    postreq.end();
+  }
+
+  function getActionId(playbookactions, name){
+    playbookactions.forEach(function(action){
+      if(action.Name == name)
+        return action.id;
+    });
+  };
+
+  function addPlaybookColumnResult(result, columnId, playbookactions){
+    var momentous = result[0] == 'm';
+    createPlaybookResult({
+      Momentous : momentous,
+      PlaybookColumnId: columnId
+    },
+    function(playbookResultRecord){
+      var order = 0;
+      for(var i = 0; i < result.length; i++){
+        var r = result[i];
+        var actionId = null;
+        switch (r) {
+          case 'm':
+            continue;
+            break;
+          case 'k':
+            actionId = getActionId(playbookactions, "Knock Down");
+            break;
+          case 'p':
+            actionId = getActionId(playbookactions, "Push");
+            break;
+          case 'd':
+            actionId = getActionId(playbookactions, "Dodge");
+            break;
+          case 't':
+            actionId = getActionId(playbookactions, "Tackle");
+            break;
+          case '1':
+            actionId = getActionId(playbookactions, "1 Damage");
+            break;
+          case '2':
+            actionId = getActionId(playbookactions, "2 Damage");
+            break;
+          case '3':
+            actionId = getActionId(playbookactions, "3 Damage");
+            break;
+          case '4':
+            actionId = getActionId(playbookactions, "4 Damage");
+            break;
+          case 'g':
+            if(result[i + 1] == "g"){
+              actionId = getActionId(playbookactions, "Character Play 1");
+              i++;
+            } else {
+              actionId = getActionId(playbookactions, "Character Play 2");
+            }
+            break;
+          default:
+
+        }
+        if(actionId){
+          createPlaybookResultAction({
+            Order: order,
+            PlaybookResultId: playbookResultRecord.Id,
+            PlaybookActionId: Id
+          })
+          order++;
+        }
+      }
+    });
+
+  };
+
+
+  function addPlaybookColumn(results, columnNumber, characterId, playbookactions){
+    if(results){
+      console.log(results);
+    createPlaybookColumn({
+       ColumnNumber: columnNumber,
+       CharacterId: characterId
+    }, function(columnRecord){
+      console.log(results);
+
+        var resultSplit = ("" + results).split(',');
+        console.log(resultSplit);
+        resultSplit.forEach(function(result){
+          addPlaybookColumnResult(result, columnRecord.id, playbookactions);
+        });
+    });
+  }
+  };
+
+  function importCharacters(playbookactions){
+    console.log(playbookactions);
+    var parser = parse({delimiter: ',', auto_parse : true}, function (err, data) {
+      async.eachSeries(data, function (line, callback) {
+          if(rowCount++ == 0) {
+            callback();
+            return;
+          }
+
+          getTeam(line[COLUMNS.TEAM], function(team){
+            console.log(team);
+            var body = JSON.stringify({
+              "Name": line[COLUMNS.NAME],
+              "MeleeZone": line[COLUMNS.MELEEZONE],
+              "Jog": line[COLUMNS.JOG],
+              "Sprint": line[COLUMNS.SPRINT],
+              "TAC": line[COLUMNS.TAC],
+              "KickDice": line[COLUMNS.KICKDICE],
+              "KickLength": line[COLUMNS.KICKLENGTH],
+              "Defense": line[COLUMNS.DEFENSE],
+              "Armor": line[COLUMNS.ARMOR],
+              "InfluenceStart": line[COLUMNS.INFLUENCESTART],
+              "InfluenceMax": line[COLUMNS.INFLUENCEMAX],
+              "IconUrl": line[COLUMNS.NAME],
+              "Health": line[COLUMNS.HEALTH],
+              "IcySponge": line[COLUMNS.ICYSPONGE],
+              "TeamId": team.id
+            });
+
+            var createOptions = {
+              host: "localhost",
+              port: 3000,
+              path: "/api/characters",
+              method: 'POST',
+              headers: {
+               "Content-Type": "application/json",
+               "Content-Length": Buffer.byteLength(body)
+             }
+            };
+
+
+            var postreq = http.request(createOptions, function(res) {
+              res.on('data', function (chunk) {
+                var character = JSON.parse(chunk);
+                console.log("character created:");
+                console.log(character);
+                for(let i = 1; i < 9; i++){
+                  if(line[COLUMNS["PB" + i]])
+                    addPlaybookColumn(line[COLUMNS["PB" + i]], i, character.id, playbookactions);
+                }
+                res.setEncoding('utf8');
+                callback();
+             });
+
+            });
+
+            postreq.write(body);
+            postreq.end();
+          });
+      })
+    })
+    fs.createReadStream(inputFile).pipe(parser);
+  }
+
+  getPlaybookActions(importCharacters);
 }
--- GuildBallImport/index.js
+++ GuildBallImport/index.js
@@ -1,5 +1,7 @@
 var characterImport = require("./imports/character-import.js");
 var teamImport = require("./imports/team-import.js");
+var playBookActionImport = require("./imports/playbook-action-import.js");
 
 //teamImport();
+//playBookActionImport();
 characterImport();
